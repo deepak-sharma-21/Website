15d38fc53fc3fa815c9362e87cb08f42cfb48dcd
deepak-sharma-21 <deepak.mailme7@gmail.com>
2022-03-16 16:25:57 +0530

    2nd Project was added

--- portfolio/src/components/Cards.jsx
+++ portfolio/src/components/Cards.jsx
@@ -3,12 +3,6 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import styles from './cards.module.css';
 import Link from '@mui/material/Link';
-
-// { title: "Hotel Website clone",duration:"1 Week",
-//  image: project1, 
-// description: "Website showcasing the luxury hotel services",
-// features:"Website consists of 5 pages (Home, Room, Dining, aboutus & Contacts) each having their own functionalities ", 
-// techStack: "HTML, CSS,JavaScript", responsibilites:"implementation of forms and extraction and storing of data"}
  
 
 export const Cards = ({data,height,width,type}) => {
@@ -25,7 +19,7 @@ export const Cards = ({data,height,width,type}) => {
           {type === "skill" ? null : <p> <span className={styles.projectTitle}>Tech Stack :-</span> {data.techStack}</p>}
           {type === "skill" ? null : <p> <span className={styles.projectTitle}>Areas of Responsibilites :-</span> {data.responsibilites}</p>}
           </div>
-        </div>
+        
         {type === "skill" ? null :
           <div className={styles.projectLinkdiv}>
             <Link href={data.codeLink} target={"_blank"} underline="hover">
@@ -36,6 +30,7 @@ export const Cards = ({data,height,width,type}) => {
             </Link>
           </div>
         }
+        </div>
         
       </CardContent>
     </Card>
--- portfolio/src/components/Project.jsx
+++ portfolio/src/components/Project.jsx
@@ -2,10 +2,32 @@ import React from 'react'
 import { Cards } from './Cards'
 import styles from './sections.module.css'
 import project1 from '../photos/Screenshot 2022-03-15 022258.png'
+import project2 from '../photos/Screenshot 2022-03-16 155508.png'
 export const Project = () => {
 
   const projects = [
-    { title: "Hotel Website clone",duration:"1 Week", image: project1, description: "Website showcasing the luxury hotel services",features:"Website consists of 5 pages (Home, Room, Dining, aboutus & Contacts) each having their own functionalities ", techStack: "HTML, CSS,JavaScript", responsibilites:"implementation of forms and extraction and storing of data",projectLink:"https://hotel-website-clone.herokuapp.com/",codeLink:"https://github.com/bhaskarkrp/Travel_clone"}
+    {
+      title: "Hotel Website clone",
+      duration: "1 Week",
+      image: project1,
+      description: "Website showcasing the luxury hotel services",
+      features: "Website consists of 5 pages (Home, Room, Dining, aboutus & Contacts) each having their own functionalities ",
+      techStack: "HTML, CSS,JavaScript",
+      responsibilites: "implementation of forms and extraction and storing of data",
+      projectLink: "https://hotel-website-clone.herokuapp.com/",
+      codeLink: "https://github.com/bhaskarkrp/Travel_clone"
+    },
+    {
+      title: "Facebook clone",
+      duration: "1 Week",
+      image: project2,
+      description: "Clone of a social networking site Facebook",
+      features: "Site functionality consists of Login, Register, Home, Friends, and Video pages with Posts and Authentication capabilities",
+      techStack: "HTML, CSS, JavaScript, React.js, Material UI",
+      responsibilites: "The implementation of login, signup, friends page with authentication functionality and different sections of the friends page",
+      projectLink: "https://facebook-clone-three-orpin.vercel.app/login",
+      codeLink: "https://github.com/deepak-sharma-21/Facebook-clone"
+    }
   ]
 
   return (<div className={styles.container} id='projects'>
